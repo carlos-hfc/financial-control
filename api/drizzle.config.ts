254f4e2715a59ab50adfe9e8dfb7615a15425a33
Carlos Faustino <chfcchfc96@gmail.com>
2025-08-27 15:47:02 -0300

    Initial commit

---
+++ api/drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit"
+
+import { env } from "./src/env.ts"
+
+export default defineConfig({
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  out: "./drizzle",
+  schema: "./src/database/schema.ts",
+})
