505d97ed4aba725c90a6249222f0451c48482a21
Carlos Faustino <chfcchfc96@gmail.com>
2025-08-28 12:43:59 -0300

    feat: add accounts table

--- api/src/database/schema.ts
+++ api/src/database/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+import {
+  numeric,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
@@ -14,4 +21,21 @@ export const categories = pgTable("categories", {
     .notNull()
     .references(() => users.id),
   name: text().notNull().unique(),
+})
+
+export const accountTypeRole = pgEnum("accountTypeRoles", [
+  "corrente",
+  "poupanca",
+  "credito",
+])
+
+export const accounts = pgTable("accounts", {
+  id: uuid().primaryKey().defaultRandom(),
+  userId: uuid()
+    .notNull()
+    .references(() => users.id),
+  name: text().notNull(),
+  type: accountTypeRole().notNull().default("corrente"),
+  initialBalance: numeric({ precision: 10, scale: 2 }).notNull().default("0"),
+  currentBalance: numeric({ precision: 10, scale: 2 }).notNull(),
 })
