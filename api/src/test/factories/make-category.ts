e094cfb05f6a51dbf7037818385b7da0e5d5240d
Carlos Faustino <chfcchfc96@gmail.com>
2025-09-01 15:08:16 -0300

    refactor: check if category exists

--- api/src/test/factories/make-category.ts
+++ api/src/test/factories/make-category.ts
@@ -1,4 +1,5 @@
 import { fakerPT_BR as faker } from "@faker-js/faker"
+import { and, eq } from "drizzle-orm"
 
 import { db } from "../../database/client.ts"
 import { categories } from "../../database/schema.ts"
@@ -11,14 +12,23 @@ export interface MakeCategoryParams {
 
 export async function makeCategory(override: Partial<MakeCategoryParams> = {}) {
   const userId = override.userId ?? (await makeUser()).user.id
+  const name = override.name ?? faker.word.adjective()
+
+  const exists = await db
+    .select()
+    .from(categories)
+    .where(and(eq(categories.name, name), eq(categories.userId, userId)))
+
+  if (exists.length > 0) {
+    return { category: exists[0] }
+  }
 
   const result = await db
     .insert(categories)
     .values({
-      name: override.name ?? faker.word.adjective(),
+      name,
       userId,
     })
-    .onConflictDoNothing({ target: categories.name })
     .returning()
 
   return {
