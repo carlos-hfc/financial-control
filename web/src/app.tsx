78aa94179becd7d5467bd1ac833a89ebb6a1d051
Carlos Faustino <chfcchfc96@gmail.com>
2025-09-10 16:39:03 -0300

    feat: add libs for integration and validation

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router"
+import { Toaster } from "sonner"
 
+import { queryClient } from "./lib/react-query"
 import { router } from "./routes"
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster richColors />
+    </QueryClientProvider>
+  )
 }
