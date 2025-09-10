d31f355764d109e2e23ee45b5f26c8f033ddf902
Carlos Faustino <chfcchfc96@gmail.com>
2025-09-10 12:09:04 -0300

    refactor: update dialog size

--- web/src/components/dialog.tsx
+++ web/src/components/dialog.tsx
@@ -13,7 +13,7 @@ export function Dialog({ className, ...props }: DialogContentProps) {
       <DialogOverlay className="fixed inset-0 z-50 bg-black/50" />
       <DialogContent
         className={cn(
-          "bg-white fixed top-1/2 left-1/2 z-50 w-full max-w-7xl sm:max-w-lg -translate-1/2 rounded-lg border border-zinc-200 shadow-lg",
+          "bg-white fixed top-1/2 left-1/2 z-50 w-[calc(100%-2rem)] max-w-7xl sm:max-w-lg -translate-1/2 rounded-lg border border-zinc-200 shadow-lg",
           className,
         )}
         {...props}
