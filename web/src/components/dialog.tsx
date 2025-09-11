6dd6b45eebdfc9c156ac0df4b62e96fa0bbaf68f
Carlos Faustino <chfcchfc96@gmail.com>
2025-09-11 13:38:10 -0300

    refactor: update dialog spacing

--- web/src/components/dialog.tsx
+++ web/src/components/dialog.tsx
@@ -43,7 +43,7 @@ const DialogContent: DialogContentProps = ({
       <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-black/50" />
       <DialogPrimitive.Content
         className={cn(
-          "bg-white fixed top-1/2 left-1/2 z-50 w-[calc(100%-2rem)] max-w-7xl sm:max-w-lg -translate-1/2 rounded-lg border border-zinc-200 shadow-lg p-6 space-y-4",
+          "bg-white fixed top-1/2 left-1/2 z-50 w-[calc(100%-2rem)] max-w-7xl sm:max-w-lg -translate-1/2 rounded-lg border border-zinc-200 shadow-lg p-6 grid gap-4",
           className,
         )}
         {...props}
