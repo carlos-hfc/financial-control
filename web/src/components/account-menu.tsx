a4b48b6f3c56a2fdc999c3ce0aa90fe01085ee5f
Carlos Faustino <chfcchfc96@gmail.com>
2025-09-11 12:02:45 -0300

    feat: get user logged profile

--- web/src/components/account-menu.tsx
+++ web/src/components/account-menu.tsx
@@ -6,50 +6,86 @@ import {
   DropdownMenuPortal,
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu"
+import { useQuery } from "@tanstack/react-query"
 import { ChevronDownIcon, LogOutIcon, UserIcon } from "lucide-react"
 
+import { Dialog } from "@/components/dialog"
+import { getProfile } from "@/http/get-profile"
+
+import { Button } from "./button"
+import { ProfileDialog } from "./profile-dialog"
+import { Skeleton } from "./skeleton"
+
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryFn: getProfile,
+    queryKey: ["profile"],
+  })
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex select-none items-center gap-1 lg:gap-2 px-2 text-zinc-800 data-[state=open]:[&>svg]:rotate-180">
-          <span className="text-sm font-semibold">Carlos Faustino</span>
-
-          <ChevronDownIcon className="size-4 transition-transform" />
-        </div>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuPortal>
-        <DropdownMenuContent
-          sideOffset={8}
-          align="end"
-          className="w-56 *:leading-none z-50 max-h-(--radix-dropdown-menu-content-available-height) overflow-x-hidden overflow-y-auto rounded-md border border-zinc-200 p-1 shadow-md bg-white"
-        >
-          <DropdownMenuLabel className="flex flex-col px-2 py-1.5">
-            <span className="font-bold text-sm text-zinc-700">
-              Carlos Faustino
-            </span>
-            <span className="text-xs text-zinc-600">carlos@email.com</span>
-          </DropdownMenuLabel>
-
-          <div className="bg-zinc-200 -mx-1 my-1 h-px" />
-
-          <DropdownMenuItem className="flex items-center gap-2 rounded-sm px-2 py-1.5 select-none outline-hidden text-sm text-zinc-700 hover:bg-zinc-100">
-            <UserIcon className="size-4 shrink-0" />
-            <span>Perfil</span>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem
-            className="flex items-center gap-2 rounded-sm px-2 py-1.5 select-none outline-hidden text-sm w-full text-rose-500 hover:bg-zinc-100"
-            asChild
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="select-none data-[state=open]:[&>svg]:rotate-180"
           >
-            <button>
-              <LogOutIcon className="size-4 shrink-0" />
-              <span>Sair</span>
-            </button>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenuPortal>
-    </DropdownMenu>
+            {isLoadingProfile ? (
+              <Skeleton className="w-20 h-4" />
+            ) : (
+              profile?.name
+            )}
+            <ChevronDownIcon className="size-4 transition-transform" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuPortal>
+          <DropdownMenuContent
+            sideOffset={8}
+            align="end"
+            className="w-56 *:leading-none z-50 max-h-(--radix-dropdown-menu-content-available-height) overflow-x-hidden overflow-y-auto rounded-md border border-zinc-200 p-1 shadow-md bg-white"
+          >
+            <DropdownMenuLabel className="flex flex-col px-2 py-1.5">
+              {isLoadingProfile ? (
+                <>
+                  <Skeleton className="w-24 h-4" />
+                  <Skeleton className="w-32 h-3 mt-1" />
+                </>
+              ) : (
+                <>
+                  <span className="font-bold text-sm text-zinc-700">
+                    {profile?.name}
+                  </span>
+                  <span className="text-xs text-zinc-600">
+                    {profile?.email}
+                  </span>
+                </>
+              )}
+            </DropdownMenuLabel>
+
+            <div className="bg-zinc-200 -mx-1 my-1 h-px" />
+
+            <Dialog.Trigger asChild>
+              <DropdownMenuItem className="flex items-center gap-2 rounded-sm px-2 py-1.5 select-none outline-hidden text-sm text-zinc-700 hover:bg-zinc-100 font-medium">
+                <UserIcon className="size-4 shrink-0" />
+                <span>Perfil</span>
+              </DropdownMenuItem>
+            </Dialog.Trigger>
+
+            <DropdownMenuItem
+              className="flex items-center gap-2 rounded-sm px-2 py-1.5 select-none outline-hidden text-sm w-full text-rose-500 hover:bg-zinc-100 font-medium"
+              asChild
+            >
+              <button>
+                <LogOutIcon className="size-4 shrink-0" />
+                <span>Sair</span>
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+
+      <ProfileDialog />
+    </Dialog>
   )
 }
