a95c8f4768f571e16ce48b151acdb429ec8c7a85
Carlos Faustino <chfcchfc96@gmail.com>
2025-10-22 16:44:02 -0300

    refactor: update list transactions with pagination

--- api/src/routes/transaction/list-transactions.spec.ts
+++ api/src/routes/transaction/list-transactions.spec.ts
@@ -7,9 +7,35 @@ import { makeCategory } from "../../test/factories/make-category.ts"
 import { makeTransaction } from "../../test/factories/make-transaction.ts"
 import { createAndAuthUser } from "../../test/utils/create-and-auth-user.ts"
 
+let token: string[]
+let accountId: string
+let categoryId: string
+let userId: string
+
 describe("List transactions [GET] /transactions", () => {
   beforeAll(async () => {
     await app.ready()
+
+    const user = await createAndAuthUser(app)
+
+    token = user.token
+    userId = user.user.id
+
+    const [{ account }, { category }] = await Promise.all([
+      makeAccount({ userId }),
+      makeCategory({ userId }),
+    ])
+
+    accountId = account.id
+    categoryId = category.id
+
+    for (let index = 0; index < 3; index++) {
+      await makeTransaction({
+        userId,
+        accountId,
+        categoryId,
+      })
+    }
   })
 
   afterAll(async () => {
@@ -17,26 +43,6 @@ describe("List transactions [GET] /transactions", () => {
   })
 
   it("should be able to list transactions", async () => {
-    const { token, user } = await createAndAuthUser(app)
-
-    const [{ account }, { category }] = await Promise.all([
-      makeAccount({ userId: user.id }),
-      makeCategory({ userId: user.id }),
-    ])
-
-    await Promise.all([
-      makeTransaction({
-        userId: user.id,
-        accountId: account.id,
-        categoryId: category.id,
-      }),
-      makeTransaction({
-        userId: user.id,
-        accountId: account.id,
-        categoryId: category.id,
-      }),
-    ])
-
     const response = await request(app.server)
       .get("/transactions")
       .set("Cookie", token)
@@ -44,22 +50,146 @@ describe("List transactions [GET] /transactions", () => {
 
     expect(response.status).toEqual(200)
     expect(response.body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(String),
-          accountId: account.id,
-          categoryId: category.id,
-          userId: user.id,
-          category: expect.objectContaining({
-            id: category.id,
-            userId: user.id,
+      expect.objectContaining({
+        transactions: expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+            accountId,
+            categoryId,
+            userId,
+            category: expect.objectContaining({
+              id: categoryId,
+              userId,
+            }),
+            account: expect.objectContaining({
+              id: accountId,
+              userId,
+            }),
           }),
-          account: expect.objectContaining({
-            id: account.id,
-            userId: user.id,
-          }),
+        ]),
+        meta: expect.objectContaining({
+          totalCount: 3,
+          pageIndex: 0,
+          perPage: 10,
         }),
-      ]),
+      }),
+    )
+  })
+
+  it("should be able to list transactions filtering by type", async () => {
+    const response = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", token)
+      .query({ type: "income" })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        transactions: expect.toBeOneOf([
+          [],
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              accountId,
+              categoryId,
+              userId,
+              type: "income",
+              category: expect.objectContaining({
+                id: categoryId,
+                userId,
+              }),
+              account: expect.objectContaining({
+                id: accountId,
+                userId,
+              }),
+            }),
+          ]),
+        ]),
+        meta: expect.objectContaining({
+          totalCount: expect.any(Number),
+          pageIndex: 0,
+          perPage: 10,
+        }),
+      }),
+    )
+  })
+
+  it("should be able to list transactions filtering by category", async () => {
+    const response = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", token)
+      .query({ category: categoryId })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        transactions: expect.toBeOneOf([
+          [],
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              accountId,
+              categoryId,
+              userId,
+              category: expect.objectContaining({
+                id: categoryId,
+                userId,
+              }),
+              account: expect.objectContaining({
+                id: accountId,
+                userId,
+              }),
+            }),
+          ]),
+        ]),
+        meta: expect.objectContaining({
+          totalCount: 3,
+          pageIndex: 0,
+          perPage: 10,
+        }),
+      }),
+    )
+  })
+
+  it("should be able to paginated the transactions listing", async () => {
+    for (let index = 0; index < 20; index++) {
+      await makeTransaction({
+        userId,
+        accountId,
+        categoryId,
+      })
+    }
+
+    const response = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", token)
+      .query({ pageIndex: 1 })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        transactions: expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+            accountId,
+            categoryId,
+            userId,
+            category: expect.objectContaining({
+              id: categoryId,
+              userId,
+            }),
+            account: expect.objectContaining({
+              id: accountId,
+              userId,
+            }),
+          }),
+        ]),
+        meta: expect.objectContaining({
+          totalCount: 23,
+          pageIndex: 1,
+          perPage: 10,
+        }),
+      }),
     )
   })
 })
--- api/src/routes/transaction/list-transactions.ts
+++ api/src/routes/transaction/list-transactions.ts
@@ -1,8 +1,15 @@
+import { and, asc, count, desc, eq } from "drizzle-orm"
 import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import z from "zod"
 
 import { db } from "../../database/client.ts"
-import { accountTypeRole, transactionTypeRole } from "../../database/schema.ts"
+import {
+  accounts,
+  accountTypeRole,
+  categories,
+  transactions,
+  transactionTypeRole,
+} from "../../database/schema.ts"
 import { auth } from "../../middlewares/auth.ts"
 
 export const listTransactionsRoute: FastifyPluginAsyncZod = async app => {
@@ -15,36 +22,44 @@ export const listTransactionsRoute: FastifyPluginAsyncZod = async app => {
         querystring: z.object({
           type: z.enum(transactionTypeRole.enumValues).optional(),
           category: z.uuid().optional(),
+          pageIndex: z.coerce.number().default(0),
         }),
         response: {
           200: z
-            .array(
-              z.object({
-                id: z.uuid(),
-                accountId: z.uuid(),
-                categoryId: z.uuid(),
-                userId: z.uuid(),
-                value: z.number(),
-                type: z.enum(transactionTypeRole.enumValues),
-                description: z.string(),
-                date: z.string(),
-                createdAt: z.date(),
-                category: z.object({
-                  id: z.uuid(),
-                  userId: z.uuid(),
-                  name: z.string(),
-                  createdAt: z.date(),
-                }),
-                account: z.object({
-                  id: z.uuid(),
-                  userId: z.uuid(),
-                  name: z.string(),
-                  type: z.enum(accountTypeRole.enumValues),
-                  currentBalance: z.number(),
-                  createdAt: z.date(),
-                }),
+            .object({
+              meta: z.object({
+                totalCount: z.number(),
+                pageIndex: z.number(),
+                perPage: z.number(),
               }),
-            )
+              transactions: z.array(
+                z.object({
+                  id: z.uuid(),
+                  accountId: z.uuid(),
+                  categoryId: z.uuid(),
+                  userId: z.uuid(),
+                  value: z.number(),
+                  type: z.enum(transactionTypeRole.enumValues),
+                  description: z.string(),
+                  date: z.string(),
+                  createdAt: z.date(),
+                  category: z.object({
+                    id: z.uuid(),
+                    userId: z.uuid(),
+                    name: z.string(),
+                    createdAt: z.date(),
+                  }),
+                  account: z.object({
+                    id: z.uuid(),
+                    userId: z.uuid(),
+                    name: z.string(),
+                    type: z.enum(accountTypeRole.enumValues),
+                    currentBalance: z.number(),
+                    createdAt: z.date(),
+                  }),
+                }),
+              ),
+            })
             .describe("OK"),
           400: z
             .object({
@@ -63,31 +78,60 @@ export const listTransactionsRoute: FastifyPluginAsyncZod = async app => {
     },
     async (request, reply) => {
       const { id: userId } = await request.getCurrentUser()
-      const { category, type } = request.query
+      const { category, type, pageIndex } = request.query
 
-      const result = await db.query.transactions.findMany({
-        with: {
-          account: true,
-          category: true,
-        },
-        where(fields, { eq, and }) {
-          return and(
-            eq(fields.userId, userId),
-            type && eq(fields.type, type),
-            category ? eq(fields.categoryId, category) : undefined,
-          )
-        },
-        orderBy(fields, { asc, desc }) {
-          return [
-            desc(fields.date),
-            desc(fields.createdAt),
-            asc(fields.description),
-          ]
-        },
-      })
+      const baseQuery = db
+        .select({
+          type: transactions.type,
+          id: transactions.id,
+          accountId: transactions.accountId,
+          categoryId: transactions.categoryId,
+          userId: transactions.userId,
+          value: transactions.value,
+          description: transactions.description,
+          date: transactions.date,
+          createdAt: transactions.createdAt,
+          category: {
+            id: categories.id,
+            userId: categories.userId,
+            name: categories.name,
+            createdAt: categories.createdAt,
+          },
+          account: {
+            id: accounts.id,
+            userId: accounts.userId,
+            name: accounts.name,
+            type: accounts.type,
+            currentBalance: accounts.currentBalance,
+            createdAt: accounts.createdAt,
+          },
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .innerJoin(categories, eq(transactions.categoryId, categories.id))
+        .where(
+          and(
+            eq(transactions.userId, userId),
+            type && eq(transactions.type, type),
+            category ? eq(transactions.categoryId, category) : undefined,
+          ),
+        )
 
-      return reply.status(200).send(
-        result.map(item => ({
+      const [transactionsCount] = await db
+        .select({ count: count() })
+        .from(baseQuery.as("baseQuery"))
+
+      const allTransactions = await baseQuery
+        .offset(pageIndex * 10)
+        .limit(10)
+        .orderBy(({ createdAt, date, description }) => [
+          desc(date),
+          desc(createdAt),
+          asc(description),
+        ])
+
+      return reply.status(200).send({
+        transactions: allTransactions.map(item => ({
           ...item,
           value: Number(item.value),
           account: {
@@ -95,7 +139,12 @@ export const listTransactionsRoute: FastifyPluginAsyncZod = async app => {
             currentBalance: Number(item.account.currentBalance),
           },
         })),
-      )
+        meta: {
+          pageIndex,
+          perPage: 10,
+          totalCount: transactionsCount.count,
+        },
+      })
     },
   )
 }
