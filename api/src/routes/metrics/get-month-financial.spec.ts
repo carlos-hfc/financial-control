c0d0df4b98929284d22a19167b7b696df717b50b
Carlos Faustino <chfcchfc96@gmail.com>
2025-11-05 16:28:55 -0300

    test: add get month financial test

---
+++ api/src/routes/metrics/get-month-financial.spec.ts
@@ -0,0 +1,59 @@
+import { faker } from "@faker-js/faker"
+import request from "supertest"
+import { afterAll, beforeAll, describe, expect, it } from "vitest"
+
+import { app } from "../../server.ts"
+import { makeAccount } from "../../test/factories/make-account.ts"
+import { makeCategory } from "../../test/factories/make-category.ts"
+import { makeTransaction } from "../../test/factories/make-transaction.ts"
+import { createAndAuthUser } from "../../test/utils/create-and-auth-user.ts"
+
+let token: string[]
+
+describe("List monthly financial [GET] /metrics/month-financial", () => {
+  beforeAll(async () => {
+    await app.ready()
+
+    const createUser = await createAndAuthUser(app)
+
+    token = createUser.token
+    const user = createUser.user
+
+    const { account } = await makeAccount({ userId: user.id })
+    const { category } = await makeCategory({ userId: user.id })
+
+    for (let index = 0; index < 50; index++) {
+      await makeTransaction({
+        userId: user.id,
+        accountId: account.id,
+        categoryId: category.id,
+        date: faker.date.recent({ days: 90 }).toISOString(),
+      })
+    }
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it("should be able to list monthly financial", async () => {
+    const response = await request(app.server)
+      .get("/metrics/month-financial")
+      .set("Cookie", token)
+      .send()
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        income: expect.objectContaining({
+          value: expect.any(Number),
+          diffFromLastMonth: expect.any(Number),
+        }),
+        expense: expect.objectContaining({
+          value: expect.any(Number),
+          diffFromLastMonth: expect.any(Number),
+        }),
+      }),
+    )
+  })
+})
